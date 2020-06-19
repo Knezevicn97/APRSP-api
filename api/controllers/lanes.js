857877a9be41a81e9522c00d0a0505ee58bcc0e7
Nikola <Nikola@DESKTOP-RKGBM7E>
2020-06-19 09:22:50 +0200

    added lanes controller with routes

---
+++ api/controllers/lanes.js
@@ -0,0 +1,108 @@
+const utilsScraper = require('../utils/gspnsScraper');
+
+
+function getAllLanes(req, res) {
+    "use strict";
+    let rv;
+    let async_call = 0;
+    let sve_linije = [];
+    var dan;
+    if (!req.query.rv) {
+        return res.status(400)
+            .json({'message': 'Malformed request, query parameters dan and rv are required.'});
+    } else {
+        var dani = ['R', 'S', 'N'];
+        rv = req.query.rv.toLowerCase();
+    }
+
+    if (rv !== 'rvg' && rv !== 'rvp') {
+        return res.status(400)
+            .json({'message': 'Malformed request, allowed values for query parameter rv are \'rvg\', \'rvp\''});
+    }
+    
+    for(dan of dani){
+        if (rv === 'rvg') {
+            utilsScraper.scrapeLaneCity(dan)
+            .then(lanes => {
+                async_call += 1;
+                sve_linije = sve_linije.concat(lanes);
+                if(async_call === dani.length) {
+                    res.status(200).json(arrayUnique(sve_linije).sort(function(a, b){
+                        var x = a.id.toLowerCase();
+                        var y = b.id.toLowerCase();
+                        x = x.replace(/[^0-9]/g, '');
+                        y = y.replace(/[^0-9]/g, '');
+                        var numX = parseInt(x);
+                        var numY = parseInt(y);
+                        return numX - numY;
+                    }))
+                }
+            })
+            .catch(err => res.status(500).json(err));
+        } else {
+            utilsScraper.scrapeLaneNonCity(dan)
+                .then(lanes => {
+                    async_call += 1;
+                    sve_linije = sve_linije.concat(lanes);
+                    if(async_call === dani.length) {
+                        res.status(200).json(arrayUnique(sve_linije).sort(function(a, b){
+                            var x = a.id.toLowerCase();
+                            var y = b.id.toLowerCase();
+                            x = x.replace(/[^0-9]/g, '');
+                            y = y.replace(/[^0-9]/g, '');
+                            var numX = parseInt(x);
+                            var numY = parseInt(y);
+                            return numX - numY;
+                        }))
+                    }
+                })
+                .catch(err => res.status(500).json(err));
+        }
+    }
+}
+
+
+function getLanes(req, res) {
+    "use strict";
+    let dan, rv;
+
+
+    if (!req.query.dan || !req.query.rv) {
+        return res.status(400)
+            .json({'message': 'Malformed request, query parameters dan and rv are required.'});
+    } else {
+        dan = req.query.dan.toUpperCase();
+        rv = req.query.rv.toLowerCase();
+    }
+    if (dan !== 'R' && dan !== 'S' && dan !== 'N') {
+        return res.status(400)
+            .json({'message': 'Malformed request, allowed values for query parameter dan are \'R\', \'S\', \'N\''});
+    } else if (rv !== 'rvg' && rv !== 'rvp') {
+        return res.status(400)
+            .json({'message': 'Malformed request, allowed values for query parameter rv are \'rvg\', \'rvp\''});
+    }
+
+    if (rv === 'rvg') {
+        utilsScraper.scrapeLaneCity(dan)
+            .then(lanes => res.status(200).json(lanes))
+            .catch(err => res.status(500).json(err));
+    } else {
+        utilsScraper.scrapeLaneNonCity(dan)
+            .then(lanes => res.status(200).json(lanes))
+            .catch(err => res.status(500).json(err));
+    }
+}
+
+module.exports = {getAllLanes, getLanes};
+
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i].id === a[j].id)
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+}
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 80;
+const lanesController = require('./api/controllers/lanes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
 
+app.route('/lanes').get(lanesController.getLanes);
+app.route('/all-lanes').get(lanesController.getAllLanes);
+
 app.listen(port);
 console.log('Listening on port ' + port);
+
